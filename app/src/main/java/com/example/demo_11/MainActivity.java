d7cd83c9f35816bb3f9b12cc2952b3b4852ba954
yaara <yaara3332@gmail.com>
2026-03-15 23:55:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/demo_11/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.demo_11;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+    EditText urledittext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = findViewById(R.id.webview);
+        urledittext = findViewById(R.id.URL);
+
